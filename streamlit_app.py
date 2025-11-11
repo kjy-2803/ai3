eb8249ecda8942790c0689d1e814dc382797eb4e
kjy-2803 <h098s02803@chunchon.gwe.hs.kr>
2025-11-11 14:22:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
     "texts": ["치킨은 나날이 비싸지는 메뉴입니다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+    #   "videos": ["https://www.youtube.com/watch?v=rqvDqRKO4dE"]
      },
 }
 
